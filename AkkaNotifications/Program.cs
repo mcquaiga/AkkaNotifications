e6855dc681f32111266cf5f2b32de5cc6504916d
Adam McQuaig <mcquaiga@gmail.com>
2015-09-29 14:22:21 -0700

    Async actors

--- AkkaNotifications/Program.cs
+++ AkkaNotifications/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ServiceStack.Redis;
 using Akka.Actor;
 using System;
+using System.Threading;
 
 namespace AkkaNotifications
 {
@@ -17,21 +18,35 @@ namespace AkkaNotifications
 
             var redisPool = new BasicRedisClientManager("127.0.0.1:6379");
             var redisConsumer = redisPool.GetClient();
-            var redisSubActor = MyActorSystem.ActorOf(Props.Create(() => new MyActor()));
-
+            
             using (sub = redisConsumer.CreateSubscription())
             { 
                 sub.OnMessage = (channel, msg) =>
                 {
                     //tell an actor that somethings come in
-                    redisSubActor.Tell(new Messages { Channel = channel, Message = msg } );
+                    var redisSubActor = MyActorSystem.ActorOf(Props.Create(() => new MyCoordinator(new Messages { Channel = channel, Message = msg })));
                 };
                 redisConsumer.PublishMessage(ChannelName, "Hello Adam");
-                sub.SubscribeToChannels(new string[] { ChannelName });
+
             }
 
-          
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                Thread.Sleep(200);
+                Console.WriteLine("Begin publishing messages...");
 
+                using (var redisPublisher = redisPool.GetClient())
+                {
+                    for (var i = 1; i <= 1000; i++)
+                    {
+                        var message = MessagePrefix + i;
+                        Console.WriteLine(String.Format("Publishing '{0}' to '{1}'", message, ChannelName));
+                        redisPublisher.PublishMessage(ChannelName, message);
+                    }
+                }
+            });
+
+            sub.SubscribeToChannels(new string[] { ChannelName });
             Console.ReadLine();
         }
     }
@@ -42,15 +57,61 @@ namespace AkkaNotifications
         public string Message { get; set; }
     }
 
-    public class MyActor : UntypedActor
+    public class MessageComplete
     {
-        protected override void OnReceive(object message)
+        public MessageComplete(int timeTaken)
         {
-            if (message is Messages)
+            TimeTaken = timeTaken;
+        }
+
+        public int TimeTaken { get; set; }
+    }
+
+    public class MyCoordinator : ReceiveActor
+    {
+        private IActorRef _childMessageActor;
+        private Messages _message;
+
+        public MyCoordinator(Messages message)
+        {
+            _message = message;
+            Receive<MessageComplete>(myMessage =>
             {
-                var m = (Messages)message;
-                Console.WriteLine("Received {0} from channel {1}", m.Message, m.Channel);
-            }     
+                ProcessCompletedMessage(myMessage.TimeTaken);
+            });
+        }
+        
+        protected override void PreStart()
+        {
+            _childMessageActor = Context.ActorOf(Props.Create(() => new MyChildActor()));
+            _childMessageActor.Tell(new Messages { Channel = _message.Channel, Message = _message.Message });
+        }
+
+        private void ProcessCompletedMessage(int time)
+        {
+            Console.WriteLine(time);
+        }
+    }
+
+    public class MyChildActor : ReceiveActor
+    {
+        public MyChildActor()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Receive<Messages>(message =>
+            {
+                ProcessMessage(string.Format("Received '{0}' from channel '{1}'", message.Message, message.Channel));
+            });
+        }
+
+        private void ProcessMessage(string message)
+        {
+            Thread.Sleep(100);
+            Console.WriteLine(message);
         }
     }
 }
